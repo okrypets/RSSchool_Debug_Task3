c3cdf110dae7f8a33085e02422788dca70d914c8
Olga Studzenichnik <okrypets@gmail.com>
2021-05-21 19:22:36 +0300

    fix: add createServer and listen port

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 const db = require('./db').sequelize; 
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
+app.use(express.json());
+
+const hostname = process.env.DB_HOST;
+const port = process.env.PORT;
+
+http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  console.log(`Server running at http://${hostname}:${port}/`);
+  res.end();
+}).listen(port, () => {
+  console.log("App is listening on 4000");
+});
 
 db.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
+
+module.exports = app
