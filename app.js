6aa2a492da7e460868ba4f847c06c9bbd5f0b431
Olga Studzenichnik <okrypets@gmail.com>
2021-05-21 19:41:04 +0300

    fix: remove deprecated body-parser and app.listen

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const db = require('./db').sequelize;
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
-app.use(express.json());
-
 const hostname = process.env.DB_HOST;
 const port = process.env.PORT;
 
@@ -15,14 +13,23 @@ http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
   console.log(`Server running at http://${hostname}:${port}/`);
   res.end();
-}).listen(port, () => {
-  console.log("App is listening on 4000");
-});
+})
 
 db.sync();
-app.use(require('body-parser'));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
+
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
+
+app.listen(port, () => {
+  console.log("App is listening on 4000");
+});
+
 
 module.exports = app
